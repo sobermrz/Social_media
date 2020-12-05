9deca0b0e5743905d4828c7bebcb213ed0469e8c
sobermrz <ma.ru@northeastern.edu>
2020-12-05 17:13:30 -0500

    2020-12-05

--- DEVCONNECTOR/client/src/components/auth/Register.js
+++ DEVCONNECTOR/client/src/components/auth/Register.js
@@ -1,32 +1,29 @@
 import React, { Fragment, useState } from "react"
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import {setAlert} from '../../actions/alert'
-import PropTypes from 'prop-types'
+import { Link, Redirect } from "react-router-dom"
+import { connect } from "react-redux"
+import { setAlert } from "../../actions/alert"
+import { register } from "../../actions/auth"
+import PropTypes from "prop-types"
 
-const Register = (setAlert) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  })
 
-  const [formData, setFormData] = useState(
-    {
-      name: '',
-      email: '',
-      password: '',
-      password2: ''
-    }
-  )
+  const { name, email, password, password2 } = formData
 
-  const {name, email, password, password2} = formData
-
-  const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
   const onSubmit = async e => {
     e.preventDefault()
 
-    if(password !== password2){
+    if (password !== password2) {
       setAlert("password do not match", "danger")
-    }else{
-      console.log('Success')
-
+    } else {
+      register({ name, email, password })
       // const newUser = {
       //   name,
       //   email,
@@ -47,8 +44,12 @@ const Register = (setAlert) => {
       // } catch (err) {
       //   console.log(err.response.data)
       // }
-
     }
+  }
+
+  //redirect to dashboard page if authenticated
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />
   }
 
   return (
@@ -62,26 +63,56 @@ const Register = (setAlert) => {
           <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
-          <small className="form-text">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a Gravatar email
+          </small>
         </div>
         <div className="form-group">
-          <input type="password" placeholder="Password" name="password" value={password} onChange={e => onChange(e)} minLength="6" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
         </div>
         <div className="form-group">
-          <input type="password" placeholder="Confirm Password" name="password2" value={password2} onChange={e => onChange(e)} minLength="6" />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <Link a="/login">Sign Up</Link>
+        Already have an account? <Link to="/login">Sign Up</Link>
       </p>
     </Fragment>
   )
 }
 
-Register.protoType =  {
+Register.protoType = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired
 }
 
-export default connect(null, {setAlert})(Register)
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+//connect Register with Redux
+export default connect(mapStateToProps, { setAlert, register })(Register)
--- DEVCONNECTOR/client/src/reducers/alert.js
+++ DEVCONNECTOR/client/src/reducers/alert.js
@@ -5,13 +5,13 @@ const initialState = []
 
 //action include the new data, nad will be updated to initialState
 export default function(state = initialState, action){
-  const {type, payload} = action
+  const {type, payload} = action//comes from action/alert.js
 
   switch(type) {
     case SET_ALERT:
-       return [...state, payload]
+       return [...state, payload]//update the state with new payload
     case REMOVE_ALERT:
-      return state.filter(alert => alert.id !== payload)
+      return state.filter(alert => alert.id !== payload)//return all alert except with the payload alert
     default:
       return state
   }
