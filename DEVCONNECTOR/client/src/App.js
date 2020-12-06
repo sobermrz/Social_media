4497a0c1d677e22afe727a75e9f79ca8f9595fd2
sobermrz <ma.ru@northeastern.edu>
2020-12-06 14:37:58 -0500

    2020-12-6

--- DEVCONNECTOR/client/src/App.js
+++ DEVCONNECTOR/client/src/App.js
@@ -2,8 +2,8 @@ import React, { Fragment, useEffect } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import "./App.css"
 import Login from "./components/auth/Login"
-import { Landing } from "./components/layout/Landing"
-import { Navbar } from "./components/layout/Navbar"
+import Landing from "./components/layout/Landing"
+import Navbar from "./components/layout/Navbar"
 import Register from "./components/auth/Register"
 import Alert from "./components/layout/Alert"
 //redux
@@ -11,6 +11,8 @@ import { Provider } from "react-redux"
 import store from "./store"
 import setAuthToken from "./utils/setAuthToken"
 import { loadUser } from "./actions/auth"
+import Dashboard from "./components/dashboard/Dashboard"
+import PrivateRoute from "./components/routing/PrivateRoute"
 
 if (localStorage.token) {
   setAuthToken(localStorage.token)
@@ -32,6 +34,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
--- DEVCONNECTOR/client/src/reducers/auth.js
+++ DEVCONNECTOR/client/src/reducers/auth.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 /* eslint-disable default-case */
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from "../actions/types"
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from "../actions/types"
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -32,11 +32,12 @@ export default function (state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem("token")
       return {
         ...state,
         token: null,
-        isAuthenticated: true,
+        isAuthenticated: false,
         loading: false
       }
     default:
